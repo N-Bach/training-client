d9499f579413006442ebffe16f15f5376a0509a0
tuan <vanhtuan0409@gmail.com>
2017-05-15 16:58:21 +0700

    add callback handle

--- src/CallbackPage.js
+++ src/CallbackPage.js
@@ -1,6 +1,17 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 export default class CallbackPage extends Component {
+    componentDidMount() {
+        const params = new URLSearchParams(this.props.location.search);
+        const code = params.get('code');
+        axios.post(`localhost:8000/api/oauth`, {
+            params: {
+                code,
+            }
+        })
+    }
+
     render() {
         return (
             <div>Callback</div>
