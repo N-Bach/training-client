66a6cb370f1815f5627964282eb5ed340da5edf4
tuan <vanhtuan0409@gmail.com>
2017-05-16 09:50:21 +0700

    update request url

--- src/CallbackPage.js
+++ src/CallbackPage.js
@@ -5,7 +5,7 @@ export default class CallbackPage extends Component {
     componentDidMount() {
         const params = new URLSearchParams(this.props.location.search);
         const code = params.get('code');
-        axios.post(`localhost:8000/api/oauth`, {
+        axios.post(`http://localhost:8000/api/oauth`, {
             params: {
                 code,
             }
